5e7450f403ae9f80a2e50a565cc35be81524906b
Samuel Jędrzejewski <samijedrzejewski@gmail.com>
2024-06-22 13:44:13 +0200

    Added Swift Moves upgrade

---
+++ TopPath/SwiftMoves/SwiftMoves.cs
@@ -0,0 +1,31 @@
+﻿using BTD_Mod_Helper.Api.Towers;
+using BTD_Mod_Helper.Extensions;
+using Il2CppAssets.Scripts.Models.Towers;
+using Il2CppAssets.Scripts.Models.Towers.Behaviors.Abilities.Behaviors;
+using Il2CppAssets.Scripts.Models.Towers.Behaviors.Abilities;
+using Il2CppAssets.Scripts.Unity;
+using Il2CppInterop.Runtime.InteropTypes.Arrays;
+using static FootballerMonkeyMod.FootballerMonkeyMod;
+
+namespace FootballerMonkeyMod.TopPath.SwiftMoves
+{
+    internal class SwiftMoves : ModUpgrade<FootballerMonkey>
+    {
+        public override int Path => TOP;
+
+        public override int Tier => 1;
+
+        public override int Cost => 300;
+
+        public override string Description => "Gives attack speed";
+
+        public override void ApplyUpgrade(TowerModel towerModel)
+        {
+            var attackModel = towerModel.GetAttackModel();
+            var weaponModel = towerModel.GetWeapon(); // You can change this is towerModel.GetAttackModel().weapons[0];
+            var projectileModel = weaponModel.projectile;
+
+            weaponModel.rate *= 0.5f;
+        }
+    }
+}
